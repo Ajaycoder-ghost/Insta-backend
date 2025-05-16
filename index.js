f6e365b5975c951ac51bcb8468d9ad994159e36f
Ajaycoder-ghost <musicspotify771@gmail.com>
2025-05-16 07:37:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(cors({
@@ -28,4 +29,6 @@ app.post("/api/instagram", async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
